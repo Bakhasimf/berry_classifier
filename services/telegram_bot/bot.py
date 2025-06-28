0ce9ade887456da3ea131083f70872c1332588ba
Bakhtiyar Kasimov <bkasimov123@gmail.com>
2025-06-28 21:25:59 +0300

    Bot token

--- services/telegram_bot/bot.py
+++ services/telegram_bot/bot.py
@@ -17,7 +17,7 @@ logging.config.dictConfig(config)
 logger = logging.getLogger("bot")
 
 # Токен бота
-BOT_TOKEN = '8098658423:AAE8jPQr2DVdtUL_PB5Ez0UmtbIGyQlCnEc'
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def main():
@@ -44,38 +44,3 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
-
-# import asyncio
-# import logging
-# from aiogram import Bot, Dispatcher
-# from aiogram.enums import ParseMode
-# from aiogram.client.default import DefaultBotProperties
-# from handlers import router
-# import logging.config
-# import yaml
-# import os
-#
-# # Логгирование
-# config_path = os.path.join(os.path.dirname(__file__), "..", "..", "logging_config.yaml")
-# with open(config_path, 'r') as f:
-#     config = yaml.safe_load(f)
-# logging.config.dictConfig(config)
-#
-#
-# BOT_TOKEN = '8098658423:AAE8jPQr2DVdtUL_PB5Ez0UmtbIGyQlCnEc'
-#
-# async def main():
-#     logging.basicConfig(level=logging.INFO)
-#
-#     bot = Bot(
-#         token=BOT_TOKEN,
-#         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-#     )
-#
-#     dp = Dispatcher()
-#     dp.include_router(router)  # 👈 подключаем router из handlers.py
-#
-#     await dp.start_polling(bot)
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
--- src/data/split.py
+++ src/data/split.py
@@ -1,16 +1,6 @@
 import splitfolders
 import os
 
-# base_dir = os.path.dirname(__file__)
-# base_dir = os.getcwd()
-# raw_path = os.path.abspath(os.path.join(base_dir, "..", "..", "data", "raw", "Berries_Fruit-262"))
-# processed_path = os.path.abspath(os.path.join(base_dir, "..", "..", "data", "processed", "Berries_Fruit-262"))
-#
-# print("RAW PATH:", raw_path)
-# print("PROCESSED PATH:", processed_path)
-# print("Содержимое папки RAW:", os.listdir(raw_path))
-
-
 base_dir = os.path.dirname(__file__)
 raw_path = os.path.join(base_dir, "..", "..", "data", "raw", "Berries_Fruit-262")
 
